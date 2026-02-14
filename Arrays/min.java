56d3d643db33cfaa7173ee53ba7687a2cc9e5093
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-14 19:12:51 +0530

    min

---
+++ Arrays/min.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class min {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr= new int[5];
+
+        System.out.println("Enter the elements of array:");
+        int min = Integer.MAX_VALUE1;
+        for(int i =0 ; i< arr.length ;i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println("The minimum element is: "+min);
+        sc.close();
+    }
+    
+}
