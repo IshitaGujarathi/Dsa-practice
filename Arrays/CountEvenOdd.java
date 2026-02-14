3aa7123fd17036dcd383a99a6d20855e115c29ec
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-14 19:22:01 +0530

    CountEvenOdd

---
+++ Arrays/CountEvenOdd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr= new int [10];
+        System.out.println("Enter the element of array:");
+        
+        int even =0;
+        int odd =0;
+        for(int i =0; i< arr.length;i++){
+            arr[i] = sc.nextInt();1
+            if(arr[i] % 2 ==0)
+                even++;
+            else
+                odd++;
+        }
+        
+        System.out.println("The odd count is:"+odd);
+        System.out.println("The even count is:"+even);
+        sc.close();
+    }
+}
