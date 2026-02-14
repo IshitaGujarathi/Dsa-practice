186e4854f6a5f98da10a6cb01065e94b91bf1370
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-14 17:56:58 +0530

    reverseArray

---
+++ Arrays/reverseArray.java
@@ -0,0 +1,10 @@
+public class reverseArray {
+    
+    public static void main(String[] args){
+        int arr[] ={1,8,7,15,90};
+        for(int i=arr.length-1; i >=0 ;i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
