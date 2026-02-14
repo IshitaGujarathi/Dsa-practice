9119decb35a0cda521772c7610a4ba6a8ccbf8f8
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-14 10:17:39 +0530

    printAll

---
+++ Arrays/printAll.java
@@ -0,0 +1,8 @@
+public class printAll {
+    public static void main(String[] args) {
+        int [] arr ={1,5,6,7,8};
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
