fadac0f5a968d0fab0cb88b8fbc8a6c2c3967b4c
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-15 20:24:49 +0530

    frequencyCount

--- Arrays/frequencyCount.java
+++ Arrays/frequencyCount.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class frequencyCount {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner sc = new Scanner (System.in);
+
+            int[] arr =new int[]{1,2,3,4,5,1,2,3,4,5};
+
+            for(int i=0;i< arr.length;i++){
+                int count=0;
+                for(int j=0;j<arr.length;j++){
+                    if(arr[i]==arr[j])
+                        count++;
+                }
+                System.out.println("the frequency of " + arr[i] + " is " + count);
+            }
+        sc.close();                
     }
 }
