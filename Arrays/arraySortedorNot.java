9977d1dfd9ce031e10bed1664ff72f72d29893d4
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-18 20:52:04 +0530

    is array is sorted or not!

---
+++ Arrays/arraySortedorNot.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class arraySortedorNot {
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,4,5,6,2};
+
+        boolean isSorted = true;
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < arr[i-1]){
+                isSorted = false;
+                break;
+            }
+        }
+
+        if(isSorted){
+            System.out.println("Array is Sorted!");
+        } else {
+            System.out.println("Array is not Sorted");
+        }
+    }
+}
