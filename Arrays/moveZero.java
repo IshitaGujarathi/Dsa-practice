e027f98d9173073827c992b7f451577ab52e9db2
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-22 15:20:23 +0530

    moveZero

--- Arrays/moveZero.java
+++ Arrays/moveZero.java
@@ -1,36 +1,34 @@
 import java.util.*;
-// Brute force Approach
 
-public class removeZeroToTheEnd {
+public class moveZero {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the size of array:");
+        System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        System.out.println("Enter the elements:");
+        System.out.println("Enter elements:");
+
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
 
-        int[] temp = new int[n];
-        int index = 0;
+        int j = 0;  
 
         for(int i = 0; i < n; i++){
             if(arr[i] != 0){
-                temp[index] = arr[i];
-                index++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                j++;
             }
         }
 
-        for(int i = 0; i < n; i++){
-            arr[i] = temp[i];
-        }
+        System.out.println("After moving zeros:");
 
-        System.out.println("Array after moving zeros to end:");
         for(int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
--- Arrays/rotateFromDth.java
+++ Arrays/rotateFromDth.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class rotateFrom1st {
+public class rotateFromDth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
