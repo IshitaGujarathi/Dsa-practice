84e359b1b4bfda991f8f42d93ef4af42e798c557
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-02-28 13:40:28 +0530

    triangularFormStar

--- Patterns/triangularFormStar.java
+++ Patterns/triangularFormStar.java
@@ -2,23 +2,20 @@ package Patterns;
 
 import java.util.*;
 
-public class printStarInDiagonalManner{
+public class triangularFormStar{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n:");
         int n = sc.nextInt();
 
-        for(int i =0; i<n ;i++){
-            for(int j =0;j<n;j++){
-            if(i==j){
-                System.out.println("* ");
+        for(int i=0;i< n ;i++){
+            for(int j=0; j<=i;j++){
+                System.out.print("* ");
             }
-            else{
-                 System.out.println();
-            }
+            System.out.println();
         }
         sc.close();
     }
-    }}
+    }
    
 
