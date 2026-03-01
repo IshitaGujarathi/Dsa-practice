b02b77d285509f689dfbdffe5fff94e2f632a957
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-01 11:26:10 +0530

    reveseNumber

---
+++ Patterns/reveseNumber.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.*;
+public class reveseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+
+        for(int i =0;i<n ;i++){
+            for(int j =1;j<n-i+1;j++){
+                System.out.print(" "+j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
