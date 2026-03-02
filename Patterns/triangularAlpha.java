f023d7041f08a89ff58325c3f8743ef3d98e38f1
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-02 11:28:52 +0530

    triangularAlpha

---
+++ Patterns/triangularAlpha.java
@@ -0,0 +1,21 @@
+package Patterns;
+import java.util.Scanner;
+
+public class triangularAlpha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            char ch = (char)('A' + i);   
+            
+            for(int j = 0; j <= i; j++){  
+                System.out.print(ch);
+            }
+            
+            System.out.println();
+        }
+        sc.close();
+    }
+}
