cb4330ae70d5be27a785f435e1b4ba833107cd26
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-03 20:51:19 +0530

    butterflySquare

---
+++ Patterns/StarWithSquareSpace.java
@@ -0,0 +1,61 @@
+package Patterns;
+import java.util.*;
+
+public class StarWithSquareSpace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        
+        int inSp = 0;
+
+        // Upper half
+        for(int i = 0; i < n; i++){
+            
+            // Left stars
+            for(int j = 0; j < n - i; j++){
+                System.out.print("*");
+            }
+            
+            // Spaces
+            for(int j = 0; j < inSp; j++){
+                System.out.print(" ");
+            }
+            
+            // Right stars
+            for(int j = 0; j < n - i; j++){
+                System.out.print("*");
+            }
+            
+            inSp += 2;
+            System.out.println();
+        }
+
+        // Reset space correctly
+        inSp = 2*n - 2;
+
+        // Lower half
+        for(int i = 1; i <= n; i++){
+            
+            // Left stars
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            
+            // Spaces
+            for(int j = 0; j < inSp; j++){
+                System.out.print(" ");
+            }
+            
+            // Right stars
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            
+            inSp -= 2;
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
