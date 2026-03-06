91f602920026ff4f79f9ed8301497a86fa6083c0
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-06 22:17:34 +0530

    longestSubArraysWithK

--- Arrays/longestSubArraysWithK.java
+++ Arrays/longestSubArraysWithK.java
@@ -3,6 +3,9 @@ public class longestSubArraysWithK {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter the value of n :");
-        int n = sc.nextInt();
+        
+
+        sc.close();
+
     }
 }
