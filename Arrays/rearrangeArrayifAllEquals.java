5d96bc97249e5310ca2c4c05dbdfe3cc3f6e2693
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-10 10:01:04 +0530

    rearrangeArrayifAllEquals

---
+++ Arrays/rearrangeArrayifAllEquals.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class rearrangeArrayifAllEquals{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("size of array:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Element of arrays are:");
+        for(int i =0;i< n ;i++){
+            arr[i]=sc.nextInt();
+        }
+    
+        int[] ans = new int[n];
+
+        int posIdx =0 , negIdx =1;
+        for(int i=0;i< n ;i++){
+            if(arr[i] < 0){
+                ans[negIdx]=arr[i];
+                negIdx +=2;
+            }
+            else{
+                ans[posIdx]=arr[i];
+                posIdx +=2;
+            }  
+    }
+    System.out.println("Rearranged Array:");
+
+        for(int i = 0; i < n; i++){
+            System.out.print(ans[i] + " ");
+        }
+    sc.close();
+    }
+
+}
