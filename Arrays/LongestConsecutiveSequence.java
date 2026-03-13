f6e170c6682c003e58633202aa2aa60d5abd1cda
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-13 22:14:58 +0530

    longestConsecutiveSequence

--- Arrays/LongestConsecutiveSequence.java
+++ Arrays/LongestConsecutiveSequence.java
@@ -1,35 +1,47 @@
 import java.util.*;
+
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the size of array:");
-        int n =  sc.nextInt();
-        System.out.println("enter the element of array :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements of array:");
         int[] arr = new int[n];
-        for(int i =0;i< n;i++){
-            arr[i]= sc.nextInt();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            sc.close();
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < n; i++){
+            set.add(arr[i]);
+        }
+
+        int longest = 0;
+
+        for(int num : set){
+
+            // check start of sequence
+            if(!set.contains(num - 1)){
+
+                int current = num;
+                int count = 1;
+
+                while(set.contains(current + 1)){
+                    current++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        System.out.println("Longest Consecutive Length: " + longest);
+
+        sc.close();
     }
 }
