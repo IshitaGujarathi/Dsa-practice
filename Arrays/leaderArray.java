adcd111f381b0d6013aea560f19d131bd15b4bc9
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-13 09:42:57 +0530

    leaderArray

--- Arrays/leaderArray.java
+++ Arrays/leaderArray.java
@@ -1,15 +1,37 @@
 import java.util.*;
-public class leaderArray{
+
+public class leaderArray {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Size of array:");
         int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
         System.out.println("Elements of array:");
-        int[] arr= new int[n];
-        for(int i=0;i< n ;i++){
-            arr[i]=sc.nextInt();
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
 
-    sc.close();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i = n - 1; i >= 0; i--){
+
+            if(arr[i] > maxi){
+                ans.add(arr[i]);
+            }
+
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        Collections.reverse(ans);
+
+        System.out.println("Leaders are: " + ans);
+
+        sc.close();
     }
 }
