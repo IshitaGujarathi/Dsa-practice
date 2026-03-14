3bb75768b7abff3d68b5640c9fb045e98b8550e2
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-14 17:18:59 +0530

    threeSum

---
+++ Arrays/threeSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class threeSum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("The size of array:");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the element of array:");
+        int[] arr = new int[n];
+        for(int i=0;i < n ;i++){
+            arr[i]= sc.nextInt();
+        }
+
+
+
+        
+    sc.close();
+    }
+
+}
