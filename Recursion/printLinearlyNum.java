2b4de82c3f0737aabe14908b125bbfa4760601a7
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-16 23:10:47 +0530

    printLinearlyNum

--- Recursion/printLinearlyNum.java
+++ Recursion/printLinearlyNum.java
@@ -1,4 +1,22 @@
 package Recursion;
+import java.util.*;
+// in this have to print 1 to n using recursion function 
 public class printLinearlyNum {
-    
+    public static void printNum(int num,int N) {
+        if(num >= N){
+            return;
+        }
+        System.out.println(num);
+        printNum(num +1, N);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of N:");
+        int N = sc.nextInt();
+        int num = 0;
+        printNum(num +1,N);
+
+        sc.close();
+
+    }
 }
