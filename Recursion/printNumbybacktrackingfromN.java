8f98c261652287f7ec45668bdce9fd001a903085
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-24 20:06:35 +0530

    printNumbybacktrackingfromN

--- Recursion/printNumbybacktrackingfromN.java
+++ Recursion/printNumbybacktrackingfromN.java
@@ -1,18 +1,24 @@
 package Recursion;
 import java.util.*;
+
 public class printNumbybacktrackingfromN {
-    public class printVal(int n , int i ){
-        if(i==0){
-            
+
+    public static void printVal(int i, int N) {
+        if (i > N) {
             return;
         }
-        printVal(N  , i+1);
-        System.out.println(i);
+        
+        printVal(i + 1, N);   
+        System.out.println(i); 
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of N: ");
         int N = sc.nextInt();
-        int i ;
-        printVal(N-1, i);
+
+        printVal(1, N);
+
+        sc.close();
     }
 }
