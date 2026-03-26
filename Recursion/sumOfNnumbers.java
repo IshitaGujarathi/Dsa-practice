01455d9e04586f2dd17a62467d62c6302f1178cb
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-26 15:05:44 +0530

    SumOfNnumber

--- Recursion/sumOfNnumbers.java
+++ Recursion/sumOfNnumbers.java
@@ -2,36 +2,39 @@ package Recursion;
 import java.util.*;
 // parameterised 
 public class sumOfNnumbers {
-    // public static void printSum(int i ,int Sum){
-    //     if(i == 0){
-    //         System.out.println(Sum);
-    //         return;
-    //     }
-    //     printSum(i-1 ,Sum+i);
-    // }
-    // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.println("Enter the value of N:");
-    //     int N = sc.nextInt();
-
-    //     printSum(N ,0);
-    //     sc.close();
-    // }
-    public static int printSum(int N){
-    if(N == 0){
-        return 0;
+    public static void printSum(int i ,int Sum){
+        if(i == 0){
+            System.out.println(Sum);
+            return;
+        }
+        printSum(i-1 ,Sum+i);
     }
-    return N + printSum(N-1);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of N:");
+        int N = sc.nextInt();
 
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the value of N:");
-    int N = sc.nextInt();
+        printSum(N ,0);
+        sc.close();
+    }
 
-    System.out.println(printSum(N));
-    sc.close();
-}
+
+    // By functional Method
+//     public static int printSum(int N){
+//     if(N == 0){
+//         return 0;
+//     }
+//     return N + printSum(N-1);
+//     }
+
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter the value of N:");
+//     int N = sc.nextInt();
+
+//     System.out.println(printSum(N));
+//     sc.close();
+// }
 
 }
 
