b0e2d72916d87dbb49ac8b1b64fe43952dceb8a7
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-26 15:17:17 +0530

    ProductOfNnumber

--- Recursion/productOfNnumber.java
+++ Recursion/productOfNnumber.java
@@ -2,18 +2,32 @@ package Recursion;
 import java.util.*;
 // parameterized
 public class productOfNnumber {
-    public static void printMul(int i,int mul) {
-        if(i==1){
-            System.out.println(mul);
-            return;
+    // public static void printMul(int i,int mul) {
+    //     if(i==1){
+    //         System.out.println(mul);
+    //         return;
+    //     }
+    //     printMul(i-1 ,mul *i);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter the value of N:");
+    //     int N = sc.nextInt();
+    //     printMul(N, 1);
+    //     sc.close();
+    // }
+    public static int printMul(int N){
+        if(N == 1){
+            return 1;
         }
-        printMul(i-1 ,mul *i);
+        return N * printMul(N-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of N:");
         int N = sc.nextInt();
-        printMul(N, 1);
+
+        System.out.println(printMul(N));
         sc.close();
     }
 
