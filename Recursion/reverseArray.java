328791226781f59415ab2c47570d4fecd33af5dd
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-03-27 21:09:39 +0530

    ReverseArrayUsingRecursion

---
+++ Recursion/reverseArray.java
@@ -0,0 +1,38 @@
+package Recursion;
+import java.util.*;
+
+public class reverseArray {
+
+    public static void revArray(int[] arr, int l, int r){
+        if(l >= r){
+            return;
+        }
+
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+
+        revArray(arr, l + 1, r - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array N");
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        System.out.println("Enter the element of array:");
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        revArray(arr, 0, N - 1);
+
+        System.out.println("Reversed array:");
+        for(int i = 0; i < N; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close(); 
+    }
+}
