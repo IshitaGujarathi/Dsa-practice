7023250ad4139c62ebea0c971b8b0862f7889f60
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-05-02 20:25:58 +0530

    IdentifyingNumOccurOrNotAndIfOcuurthenHowManyTimes

--- Hashing/IdentifyingNumOccurOrNot.java
+++ Hashing/IdentifyingNumOccurOrNot.java
@@ -1,33 +1,33 @@
-package Hashing;
-import java.util.*;
-public class IdentifyingNumOccurOrNot {
-    int q;
-    System.out.println(q);
-    While(q--){
-        int num;
-        System.out.println(num);
-        return 0;
-    }
+// package Hashing;
+// import java.util.*;
+// public class IdentifyingNumOccurOrNot {
+//     int q;
+//     System.out.println(q);
+//     While(q--){
+//         int num;
+//         System.out.println(num);
+//         return 0;
+//     }
 
-}
-
-
-public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// }
 
 
-        System.out.print("Size of Array: ");
-        int n = sc.nextInt();
+// public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[n];
 
-        System.out.println("Enter elements:");
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
+//         System.out.print("Size of Array: ");
+//         int n = sc.nextInt();
 
-        int hash[n+1]={0};
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter elements:");
+//         for(int i = 0; i < n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int hash[n+1]={0};
         
 
-}
-}
+// }
+// }
