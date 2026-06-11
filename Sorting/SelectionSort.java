e544b38801757e8377838944f79b1ff71d2e0e4c
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-06-11 11:39:38 +0530

    Selection Sort 1st commit

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,3 +1,44 @@
+import java.util.*;
 public class SelectionSort {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the value of n :");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the value element of array:");
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        // selection sort 
+
+        for(int j = 0; j <= n-2;j++){
+            int min = j;
+            
+            for(int k =j ;j< n-1; k++){
+                if(arr[k] < arr[min]){
+                    min = k;
+                
+                int temp = arr[min];
+                arr[min] = arr[j];
+                arr[j] = temp;
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        sc.close();
+
+    }
 }
