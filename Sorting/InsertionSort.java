f8f429ca3762d0d13d499e1195571ce43924d063
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-06-12 21:31:06 +0530

    Insertion sort 1st commit

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class InsertionSort{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the element of the array:");
+        for(int i=0;i< n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
