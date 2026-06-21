418e48893ebe6474e9b289b8573729687dc322a0
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-06-21 21:33:31 +0530

    Quick Sort

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,11 +1,72 @@
+import java.util.*;
+
 public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+
+            while (i <= high - 1 && arr[i] <= pivot) {
+                i++;
+            }
+
+            while (j >= low + 1 && arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+
+        if (low >= high) {
+            return;
+        }
+
+        int partitionIndex = partition(arr, low, high);
+
+        quickSort(arr, low, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, high);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new scanner(system.in);
-        System.out.println("Enter the size of the array:")
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
         int n = sc.nextInt();
 
-        int[] arr = new Int[n];
-        System.out.println("Enter the element of array:");
-        
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted Array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
     }
 }
