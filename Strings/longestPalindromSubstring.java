1449270d0533212ad32aaa862ff03e541e2efcaa
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-06-24 21:01:22 +0530

    longestPlaindromeSubstring

---
+++ Strings/longestPalindromSubstring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class longestPalindromSubstring{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String S =new String();
+        for(int i=0 ;i< S.length;i++){
+            String S = sc.nextLine();
+        }
+
+        sc.close();
+
+    }
+}
