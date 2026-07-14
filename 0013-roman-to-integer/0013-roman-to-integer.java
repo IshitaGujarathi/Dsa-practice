1c5d25523aec3386d8d4b9dd554df57a5d8a891d
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-07-14 22:07:15 +0530

    Time: 2 ms (100%), Space: 46.4 MB (75.62%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,26 +1,14 @@
-import java.util.HashMap;
-
 class Solution {
+
     public int romanToInt(String s) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
 
         int result = 0;
 
         for (int i = 0; i < s.length(); i++) {
 
-            int current = map.get(s.charAt(i));
+            int current = value(s.charAt(i));
 
-            // check if current value is smaller than next value
-            if (i + 1 < s.length() && current < map.get(s.charAt(i + 1))) {
+            if (i + 1 < s.length() && current < value(s.charAt(i + 1))) {
                 result -= current;
             } 
             else {
@@ -29,5 +17,19 @@ class Solution {
         }
 
         return result;
+    }
+
+    private int value(char c) {
+
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
