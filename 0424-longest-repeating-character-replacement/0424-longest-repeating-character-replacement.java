d4045de13e41da532b64049311d2cce432fa4024
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-07-16 18:02:44 +0530

    Time: 6 ms (98.86%), Space: 46.4 MB (31.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,24 +1,21 @@
-import java.util.HashMap;
-
-class Solution {
+public class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> freqs = new HashMap<>();
-        int res = 0, i = 0, maxFreq = 0;
-
-        for (int j = 0; j < s.length(); j++) {
-            char c = s.charAt(j);
-            freqs.put(c, freqs.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, freqs.get(c));
-
-            while ((j - i + 1) - maxFreq > k) {
-                char left = s.charAt(i);
-                freqs.put(left, freqs.get(left) - 1);
-                i++;
+        char[] sc = s.toCharArray();
+        int[] count = new int[26];
+        int maxCount = 0, start = 0;
+        for(int i=0;i<sc.length;i++){
+            int index = sc[i]-'A';
+            count[index]++;
+            maxCount = Math.max(maxCount, count[index]);
+            // i - start means the current longest length, if the (maxCount + k) <= (the current longest length), we move whole windows right.
+            // otherwise the start stays at current position, but the "i" moves, means next round the longest length will increase.
+            if(maxCount + k <= i - start){
+                int indexStart = sc[start] - 'A';
+                count[indexStart]--;
+                start++;
             }
-
-            res = Math.max(res, j - i + 1);
         }
-
-        return res;
+        
+        return sc.length-start;
     }
 }
