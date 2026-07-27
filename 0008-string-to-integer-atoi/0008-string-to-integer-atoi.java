13c16e223c53c1b40c3b4dc323a1798eec2747c7
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-07-27 11:36:02 +0530

    Time: 1 ms (100%), Space: 43.8 MB (59.88%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -4,24 +4,20 @@ class Solution {
             return 0;
         }
         
-        // Constants for 32-bit signed integer range
         final int INT_MAX = Integer.MAX_VALUE;
         final int INT_MIN = Integer.MIN_VALUE;
         
         int i = 0;
         int n = s.length();
         
-        // Step 1: Skip leading whitespace
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
         
-        // Check if we've reached the end
         if (i == n) {
             return 0;
         }
         
-        // Step 2: Check for sign
         int sign = 1;
         if (s.charAt(i) == '+') {
             i++;
@@ -30,7 +26,6 @@ class Solution {
             i++;
         }
         
-        // Step 3: Read digits and convert
         long res = 0;
         while (i < n && Character.isDigit(s.charAt(i))) {
             int digit = s.charAt(i) - '0';
@@ -46,7 +41,6 @@ class Solution {
             i++;
         }
         
-        // Step 4: Apply sign and return
         return (int)(res * sign);        
     }
 }
