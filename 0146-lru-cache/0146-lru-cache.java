0dc92a354f23149351286a1fb7481b42a3b200cb
Ishita Gujarathi <ishitagujarathi07@gmail.com>
2026-08-03 10:39:27 +0530

    Time: 36 ms (99.45%), Space: 129 MB (99.65%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,73 +1,105 @@
 class LRUCache {
-    class Node {
+    class Node{
         int key;
-        int val;
+        int value;
+
         Node prev;
         Node next;
 
-        Node(int key, int val) {
-            this.key = key;
-            this.val = val;
+        Node(int key, int value){
+            this.key= key;
+            this.value= value;
         }
     }
 
-    Node head = new Node(-1, -1);
-    Node tail = new Node(-1, -1);
-    int cap;
-    HashMap<Integer, Node> m = new HashMap<>();
-
+    public Node[] map;
+    public int count, capacity;
+    public Node head, tail;
+    
     public LRUCache(int capacity) {
-        cap = capacity;
-        head.next = tail;
-        tail.prev = head;
+        
+        this.capacity= capacity;
+        count= 0;
+        
+        map= new Node[10_000+1];
+        
+        head= new Node(0,0);
+        tail= new Node(0,0);
+        
+        head.next= tail;
+        tail.prev= head;
+        
+        head.prev= null;
+        tail.next= null;
     }
-
-    private void addNode(Node newnode) {
-        Node temp = head.next;
-
-        newnode.next = temp;
-        newnode.prev = head;
-
-        head.next = newnode;
-        temp.prev = newnode;
+    
+    public void deleteNode(Node node){
+        node.prev.next= node.next;
+        node.next.prev= node.prev;       
+        
+        return;
     }
-
-    private void deleteNode(Node delnode) {
-        Node prevv = delnode.prev;
-        Node nextt = delnode.next;
-
-        prevv.next = nextt;
-        nextt.prev = prevv;
+    
+    public void addToHead(Node node){
+        node.next= head.next;
+        node.next.prev= node;
+        node.prev= head;
+        
+        head.next= node;      
+        
+        return;
     }
-
+    
     public int get(int key) {
-        if (m.containsKey(key)) {
-            Node resNode = m.get(key);
-            int ans = resNode.val;
-
-            m.remove(key);
-            deleteNode(resNode);
-            addNode(resNode);
-
-            m.put(key, head.next);
-            return ans;
+        
+        if( map[key] != null ){
+            
+            Node node= map[key];
+            
+            int nodeVal= node.value;
+            
+            deleteNode(node);
+            
+            addToHead(node);
+            
+            return nodeVal;
         }
-        return -1;
+        else
+            return -1;
     }
-
+    
     public void put(int key, int value) {
-        if (m.containsKey(key)) {
-            Node curr = m.get(key);
-            m.remove(key);
-            deleteNode(curr);
+        
+        if(map[key] != null){
+            
+            Node node= map[key];
+            
+            node.value= value;
+            
+            deleteNode(node);
+            
+            addToHead(node);
+            
+        } else {
+            
+            Node node= new Node(key,value);
+            
+            map[key]= node;
+            
+            if(count < capacity){
+                count++;
+                addToHead(node);
+            } 
+            else {
+                
+                map[tail.prev.key]= null;
+                deleteNode(tail.prev);
+                
+                addToHead(node);
+            }
         }
-
-        if (m.size() == cap) {
-            m.remove(tail.prev.key);
-            deleteNode(tail.prev);
-        }
-
-        addNode(new Node(key, value));
-        m.put(key, head.next);
+        
+        return;
     }
+    
 }
